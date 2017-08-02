4a9326d349b336a4a65b91498883d2ee4c6eea21
Guido Fonticelli <guido.fonticelli@opinaiapanel.com>
2017-08-02 18:54:17 -0300

    WIP

--- src/CreadorBundle/Resources/public/js/users.js
+++ src/CreadorBundle/Resources/public/js/users.js
@@ -5,29 +5,33 @@ $("body").on('click', '.deleteUser', function(){
 
 });
 
-function addUser(userPositionName) {
+function addUser(userPositionName, removable, locale) {
+  removable = removable !== false;
+  locale = locale || '';
+
   var $body = $('.usersTypes');
 
-
-
-  $body.append('<div class="input-group">\n' +
+  var $html =
+    '<div class="input-group">\n' +
     '        <input type="text" class="form-control" name="usertypes[]" value="'+userPositionName+'">\n' +
-    '        <span class="input-group-btn">\n' +
-    '            <button class="btn btn-danger deleteUser" data-text="'+userPositionName+'" type="button"><i class="fa fa-trash" aria-hidden="true"></i>\n</button>\n' +
-    '        </span>\n' +
-    '    </div>');
+    '        <span class="input-group-btn">\n';
+
+
+  if (removable === true) {
+    $html +='<button class="btn btn-danger deleteUser" data-text="'+userPositionName+'" type="button"><i class="fa fa-trash" aria-hidden="true"></i></button>\n';
+  } else {
+    $html +='<button class="btn btn-danger deleteUser" data-text="'+userPositionName+'" type="button" disabled="disabled"><i class="fa fa-trash" aria-hidden="true"></i></button>\n';
+  }
+
+  $html += '        </span>\n' +
+    '</div>';
+
+  $body.append($html);
+  addUserPositionTrans(userPositionName, locale);
 }
-
-
 
 $("#addUserBtn").click(function(){
   var userPositionName = $('#userPositionName').val();
 
   addUser(userPositionName);
-  addUserPositionTrans(userPositionName)
 });
-
-addUserPositionTrans('REGION', 'Region');
-addUserPositionTrans('COUNTRY', 'Pais');
-addUserPositionTrans('STORE', 'Tienda');
-addUserPositionTrans('NON_HIERARCHICAL', 'NON_HIERARCHICAL');
